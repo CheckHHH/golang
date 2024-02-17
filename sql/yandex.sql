61aedb93dad1c3779665d9f54b81ac48c6b9081c
CheckHHH <150026023+CheckHHH@users.noreply.github.com>
2024-02-17 23:40:57 +0300

    Add files via upload

---
+++ sql/yandex.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+SET client_encoding = 'LATIN1';
+
+CREATE TABLE Expressions (
+    id SERIAL,
+    baseID BIGINT,
+    Expression text NOT NULL,
+    Value text,
+    Err boolean NOT NULL DEFAULT false,
+    CurrentResult text
+);
+
+CREATE TABLE Configs (
+    id SERIAL,
+    Plus BIGINT,
+    Minus BIGINT,
+    Multiply BIGINT,
+    Divide BIGINT
+    MaxID BIGINT,
+);
+
+COMMIT;
